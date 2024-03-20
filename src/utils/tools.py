4f7006cacf1b8dd7de440e486d22171b3c1c7bc3
yiannislevy <yiannis.levy@gmail.com>
2024-03-20 11:40:44 +0200

    Add save_data function

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -108,3 +108,24 @@ def plot_pickle(processed_data_directory, subject_id, title_prefix):
 
     # Plot gyroscope data
     plot_each_sensor(gyro_data, 'Gyroscope')
+
+
+def save_data(data, processed_data_directory, filename):
+    """
+    Save data to a pickle file.
+
+    Args:
+        data: Data to be saved, can be of any type.
+        processed_data_directory (str): Directory to save the data.
+        filename (str): Name of the file to save.
+    """
+    # Create the directory for the file if it doesn't exist
+    file_dir = os.path.join(processed_data_directory)
+    os.makedirs(file_dir, exist_ok=True)
+
+    # Define the file path
+    file_path = os.path.join(file_dir, f"{filename}.pkl")
+
+    # Save the data in pickle format
+    with open(file_path, 'wb') as file:
+        pickle.dump(data, file)
